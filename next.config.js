3cfe095258c075e6f048f0b2ff4f3d8f2052b35b
Arpit Kushwaha <arpit@example.com>
2026-02-04 14:40:20 +0530

    Update dependencies and fix undici compatibility issue

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['cheerio'],
+  },
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), { canvas: 'canvas' }];
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
